9f74c77b41eaaa7937a812e7857f1a5cd575b44c
Siriporn Worranawin <cheenoi30@gmail.com>
2025-07-17 11:09:45 +0700

    game-fn solutions

--- game-fn.js
+++ game-fn.js
@@ -8,18 +8,50 @@ const board = [
 
 let x = 0;
 let y = 0;
+// const moves = [];
+// moves.push(board[x][y]); // Start at A
+
+// Define movement functions
+// function moveRight() {}
+
+// function moveLeft() {}
+
+// function moveUp() {}
+
+// function moveDown() {}
+
+// Call the movement functions
+// moveRight(); // Move from A to B
+// moveRight(); // Move from B to C
+// moveDown(); // Move from C to F
+// moveLeft(); // Move from F to E
+
+// console.log("Path:", moves.join(" → "));
+// console.log("Total Moves (Start from 'A'):", moves.length - 1);
+
+const xMax = board.length - 1;
+const yMax = board[0].length -1;
 const moves = [];
 moves.push(board[x][y]); // Start at A
 
 // Define movement functions
-function moveRight() {}
+function moveRight() { 
+    y < yMax ? moves.push(board[x][++y]) : limitLog('moving right'); 
+}
+function moveLeft() { 
+    y > 0 ? moves.push(board[x][--y]) : limitLog('moving left'); 
+}
+function moveUp() { 
+    x > 0 ? moves.push(board[--x][y]) : limitLog('moving up'); 
+}
 
-function moveLeft() {}
+function moveDown() { 
+    x < xMax ? moves.push(board[++x][y]) : limitLog('moving down'); 
+}
 
-function moveUp() {}
-
-function moveDown() {}
-
+function limitLog(moving) { 
+    console.log( `You have reached the limit of ${moving}.` ); 
+}
 // Call the movement functions
 moveRight(); // Move from A to B
 moveRight(); // Move from B to C
